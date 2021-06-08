9bbb79d2f714b170b76625c5145c12d0f0e16322
Bill Yuchen Lin <yuchenlin@fb.com>
2021-06-08 12:24:58 -0700

    move the scripts

---
+++ scripts/run_mrqa.sh
@@ -0,0 +1,21 @@
+task="mrqa_squad"
+python src/cli_base.py \
+        --do_predict \
+        --output_dir out/${task} \
+        --model facebook/bart-large \
+        --checkpoint checkpoints/best-model.pt \
+        --predict_checkpoint checkpoints/best-model.pt \
+        --dataset ${task} \
+        --train_file data/${task}/${task}_train.tsv \
+        --dev_file data/${task}/${task}_dev.tsv \
+        --test_file data/${task}/${task}_dev.tsv \
+        --learning_rate 1e-5 \
+        --warmup_steps 100 \
+        --train_batch_size 16 \
+        --predict_batch_size 16 \
+        --eval_period 300 \
+        --num_train_epochs 10 \
+        --max_input_length 888 \
+        --max_output_length 50 \
+        --num_beams 3 \
+        --append_another_bos
