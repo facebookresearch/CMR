8310a8881aaa39583d15bbf9bdc219cbe7c76b1f
Bill Yuchen Lin <yuchenlin@fb.com>
2021-06-08 14:11:02 -0700

    logs

--- scripts/run_mrqa.sh
+++ scripts/run_mrqa.sh
@@ -1,19 +1,21 @@
 task="mrqa_squad"
-CUDA_VISIBLE_DEVICES=1 python src/cli_base.py \
+python src/cli_base.py \
         --do_train \
         --output_dir out/${task} \
         --model facebook/bart-large \
         --dataset ${task} \
         --train_file data/${task}/${task}_train.tsv \
-        --dev_file data/${task}/${task}_dev.tsv \
+        --dev_file data/${task}/${task}_dev.mini.tsv \
         --test_file data/${task}/${task}_dev.tsv \
         --learning_rate 1e-5 \
         --warmup_steps 100 \
-        --train_batch_size 1 \
-        --predict_batch_size 1 \
+        --train_batch_size 16 \
+        --predict_batch_size 16 \
         --eval_period 300 \
         --num_train_epochs 10 \
         --max_input_length 888 \
         --max_output_length 50 \
         --num_beams 3 \
-        --append_another_bos
+        --append_another_bos  > logs/train_${task}.log 2>&1 &
+
+tail -f logs/train_${task}.log
