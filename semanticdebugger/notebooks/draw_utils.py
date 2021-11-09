28035b811215901e75f4b2f02b15f0e38ec373ca
Bill Yuchen Lin <yuchenlin@fb.com>
2021-11-08 16:26:33 -0800

    update figures

--- semanticdebugger/notebooks/draw_utils.py
+++ semanticdebugger/notebooks/draw_utils.py
@@ -22,16 +22,17 @@ def draw_curve(df, y_scale=[0, 1], fig_title="", y_title="Y Title", x_key="timec
     return fig
 
 
-def draw_stacked_bars(df, y_scale=[0, 1], fig_title="", y_title="Y Title", x_key="timecode", y_key="em:Q", height=800, width=1150):
-    x = alt.X(x_key, type="ordinal", title="Time Step", axis=alt.Axis(tickCount=10)) # axis=alt.Axis(tickCount=10)
+def draw_stacked_bars(df, y_scale=[0, 1], fig_title="", y_title="Y Title", x_key="timecode", y_key="em:Q", height=800, width=1150, x_scale=[0, 100]):
+    
     color=alt.Color('prefix:N')  
-    fig = alt.Chart(df).mark_bar().encode(x=x, y=alt.Y(y_key, title=y_title, scale=alt.Scale(domain=y_scale)), color=color,).properties(title=fig_title)
+    fig = alt.Chart(df).mark_bar().encode(x=alt.X(x_key, title="Time Step", axis=alt.Axis(tickMinStep=10, tickOffset=0, tickWidth=1), scale=alt.Scale(domain=x_scale)), 
+                                            y=alt.Y(y_key, title=y_title, scale=alt.Scale(domain=y_scale)), 
+                                            color=color,).properties(title=fig_title)
 
     fig = alt.layer(fig).resolve_scale()
     fig = fig.properties(width=width, height=height).configure_axis(
-        labelFontSize=15,
-        titleFontSize=16, 
-        # tickMinStep=10,
+        labelFontSize=25,
+        titleFontSize=25,  
     ).configure_legend(titleFontSize=0, labelFontSize=25, orient='top-left', strokeColor='gray',
         fillColor='#EEEEEE',
         padding=5,
